d82fd6708e35d4199a2a43e590a1005fbc46754b
cosmir <cosmir@cosmir>
2021-06-12 12:17:06 +0100

    adding a spec file

--- video-asset-handler/modules/tests/src/test/scala/modules/HashHandlerConvertTest.scala
+++ video-asset-handler/modules/tests/src/test/scala/modules/HashHandlerConvertTest.scala
@@ -41,6 +41,23 @@ object HashHandlerConvertTest extends SimpleIOSuite with Checkers with HashesTes
     } yield  s1 and s256 and m5 and c
   }
 
+  test("Should yield empty strings for an empty input") {
+    val text   = ""
+    val sha1   = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+    val sha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    val md5    = "d41d8cd98f00b204e9800998ecf8427e"
+    val crc32  = "0"
+    val resource = createResource(text)
+
+    for {
+      Hashes(sha1T, sha256T, md5T, crc32T) <- resource.use(hashHandler.convert)
+      s1          = expect.same(sha1T, sha1)
+      s256        = expect.same(sha256T, sha256)
+      m5          = expect.same(md5T, md5)
+      c           = expect.same(crc32T, crc32)
+    } yield  s1 and s256 and m5 and c
+  }
+
   private def createResource(text: String) =
     Resource.make {
       IO.blocking(new BufferedInputStream(new ByteArrayInputStream(
